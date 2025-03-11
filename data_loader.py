312560ac29440828c53365d798e03f5cc169e9bf
Simon-Bae <simon.bae825@gmail.com>
2025-03-11 16:56:32 +0900

    feat: add MNISTDataset and data_loaders for train/valid/test data
    - torch의 Dataset 클래스를 기반으로 MNISTDataset 선언
    - get_loaders() -> torch의 Dataloader 클래스를 기반으로 train/valid/test 데이터로더를 불러올 수 함수 선언
    - 기존 utils.py row 데이터 불러오기 함수, 데이터 shuffle, split 함수 이동 및 get_loaders() 함수에 적용
    - 기존 utils.py 내 load_data(), split_data() 함수들 중복으로 인해 삭제

---
+++ data_loader.py
@@ -0,0 +1,92 @@
+import torch
+
+from torch.utils.data import Dataset, DataLoader
+
+
+class MNISTDataset(Dataset):
+    """ CustomDataset Class for MNIST dataset """
+    def __init__(self, data, labels, flatten=True):
+        self.data = data
+        self.labels = labels
+        self.flatten = flatten
+
+        super().__init__()
+
+    def __len__(self):
+        return self.data.size(0)
+
+    def __getitem__(self, idx):
+        x = self.data[idx]
+        y = self.labels[idx]
+
+        # flatten 28x28 image -> 784 vector
+        # |x| = (28, 28) -> |x| = (784,)
+        if self.flatten:
+            x = x.view(-1)
+
+        return x, y
+
+
+def load_mnist(is_train=True):
+    """ MNIST train/valid 데이터 불러오기 """
+    from torchvision import datasets, transforms
+
+    # load mnist data
+    dataset = datasets.MNIST(
+        root="./dataset",
+        train=is_train,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+        ]),
+        download=True
+    )
+
+    # MinMax Scaling
+    x = dataset.data.float() / 255.
+    y = dataset.targets
+
+    return x, y
+
+
+def split_data(x, y, config):
+    """ shuffle and split train/valid data """
+    # set train/valid count
+    train_cnt = int(x.size(0) * config.train_ratio)
+    valid_cnt = x.size(0) - train_cnt
+
+    # shuffle and split data into train/valid data
+    indices = torch.randperm(x.size(0))
+    x = torch.index_select(x, dim=0, index=indices).split([train_cnt, valid_cnt], dim=0)
+    y = torch.index_select(y, dim=0, index=indices).split([train_cnt, valid_cnt], dim=0)
+
+    return x[0], y[0], x[1], y[1]
+
+
+def get_loaders(config):
+    # load MNIST train data
+    x, y = load_mnist(is_train=True)
+
+    # shuffle and split data
+    train_x, train_y, valid_x, valid_y = split_data(x, y, config)
+
+    # load MNIST test data
+    test_x, test_y = load_mnist(is_train=False)
+
+    # set data loaders
+    train_loader = DataLoader(
+        dataset=MNISTDataset(train_x, train_y, flatten=True),
+        batch_size=config.batch_size,
+        shuffle=True
+    )
+    valid_loader = DataLoader(
+        dataset=MNISTDataset(valid_x, valid_y, flatten=True),
+        batch_size=config.batch_size,
+        shuffle=True
+    )
+    test_loader = DataLoader(
+        dataset=MNISTDataset(test_x, test_y, flatten=True),
+        batch_size=config.batch_size,
+        shuffle=True
+    )
+
+    return train_loader, valid_loader, test_loader
